213a01ab0a707bc4afd858fe593a0fb6e8967ecb
Mouli Chakraborty <127589681+Mouli51ch@users.noreply.github.com>
2025-05-29 00:34:18 +0530

    package.json fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,4 +9,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: {
+    enabled: true,
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+  },
 } 
